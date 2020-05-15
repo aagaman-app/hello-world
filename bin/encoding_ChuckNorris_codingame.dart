7a158f970f12a6582737012df5a9f9f8f8e0a429
aagaman-app <agam@aagaman.app>
2020-05-15 21:07:21 +0530

    dec to binary to encoding puzzle solved

--- bin/encoding_ChuckNorris_codingame.dart
+++ bin/encoding_ChuckNorris_codingame.dart
@@ -1,6 +1,64 @@
-void main () {}
-
-
 //https://www.codingame.com/training/easy/chuck-norris
 
+import 'dart:io';
+import 'dart:math';
 
+void main() {
+  String MESSAGE = stdin.readLineSync();
+
+  List<int> codeunits = MESSAGE.codeUnits;
+  // stderr.writeln(codeunits);
+  // stderr.writeln('$MESSAGE : $codeunits');
+  List<String> binary = [];
+
+  for (int codeunit in codeunits) {
+    int q = codeunit;
+    int remainder;
+    List<int> bitNumbers = [];
+    while (q > 0) {
+      remainder = q % 2;
+      q = q ~/ 2;
+      bitNumbers.add(remainder);
+      // stderr.writeln('q: ${q}, remainder: $remainder');
+    }
+    if (bitNumbers.length < 7) {
+      for (int i = 0; i < 7 - bitNumbers.length; i++) {
+        bitNumbers.add(0);
+      }
+      binary.add(bitNumbers.reversed.join());
+    } else {
+      binary.add(bitNumbers.reversed.join());
+    }
+  }
+
+  String bin = binary.join();
+  // stderr.writeln(bin);
+
+  String answer;
+  // stderr.writeln(bin.length);
+  String id;
+  String count;
+  for (int i = 0; i < bin.length; i++) {
+    if (i == 0) {
+      id = bin[i] == '0' ? '00' : '0';
+      count = '0';
+    } else {
+      if (bin[i] == bin[i - 1]) {
+        id = bin[i] == '0' ? '00' : '0';
+        count = '${count}0';
+        answer = i == bin.length - 1 ? '$answer $id $count' : answer;
+      } else {
+        answer = answer != null ? '$answer $id $count' : '$id $count';
+        id = bin[i] == '0' ? '00' : '0';
+        count = '0';
+        answer = i == bin.length - 1 ? '$answer $id $count' : answer;
+      }
+    }
+    // stderr.writeln('Step:$i: $answer');
+    // stderr.writeln('${bin[i]}: $id : $count');
+
+    // stderr.writeln('$id-$count = $answer');
+  }
+
+  print(answer);
+}
--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,62 @@
 import 'dart:io';
 import 'dart:math';
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 void main() {
-    String MESSAGE = stdin.readLineSync();
+  String MESSAGE = stdin.readLineSync();
 
-    // Write an answer using print()
-    // To debug: stderr.writeln('Debug messages...');
+  List<int> codeunits = MESSAGE.codeUnits;
+  // stderr.writeln(codeunits);
+  // stderr.writeln('$MESSAGE : $codeunits');
+  List<String> binary = [];
 
-    print('answer');
+  for (int codeunit in codeunits) {
+    int q = codeunit;
+    int remainder;
+    List<int> bitNumbers = [];
+    while (q > 0) {
+      remainder = q % 2;
+      q = q ~/ 2;
+      bitNumbers.add(remainder);
+      // stderr.writeln('q: ${q}, remainder: $remainder');
+    }
+    if (bitNumbers.length < 7) {
+      for (int i = 0; i < 7 - bitNumbers.length; i++) {
+        bitNumbers.add(0);
+      }
+      binary.add(bitNumbers.reversed.join());
+    } else {
+      binary.add(bitNumbers.reversed.join());
+    }
+  }
+
+  String bin = binary.join();
+  // stderr.writeln(bin);
+
+  String answer;
+  // stderr.writeln(bin.length);
+  String id;
+  String count;
+  for (int i = 0; i < bin.length; i++) {
+    if (i == 0) {
+      id = bin[i] == '0' ? '00' : '0';
+      count = '0';
+    } else {
+      if (bin[i] == bin[i - 1]) {
+        id = bin[i] == '0' ? '00' : '0';
+        count = '${count}0';
+        answer = i == bin.length - 1 ? '$answer $id $count' : answer;
+      } else {
+        answer = answer != null ? '$answer $id $count' : '$id $count';
+        id = bin[i] == '0' ? '00' : '0';
+        count = '0';
+        answer = i == bin.length - 1 ? '$answer $id $count' : answer;
+      }
+    }
+    // stderr.writeln('Step:$i: $answer');
+    // stderr.writeln('${bin[i]}: $id : $count');
+
+    // stderr.writeln('$id-$count = $answer');
+  }
+
+  print(answer);
 }
